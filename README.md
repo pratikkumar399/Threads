5ce66c1372f86988c956316f0ddf2aa6baa4fd25
raipratik <pratikkumar399@gmail.com>
2023-11-21 00:38:59 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## Threads Clone
+
+* This repo contains fullstack threads clone project.
+
+* Tech Stack used : React, MongoDb, NodeJs, FireBase, ChakraUi
+
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Button } from '@chakra-ui/react'
+import { Container } from '@chakra-ui/react'
+
+function App() {
+
+  return (
+    <>
+      <Container maxW='620px'>
+        <Button colorScheme="blue">Button</Button>
+      </Container>
+    </>
+  )
+}
+
+export default App
